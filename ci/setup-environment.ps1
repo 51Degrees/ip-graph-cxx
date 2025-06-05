e802edf7d8f681180d7e173bb51a758ffd505707
Alexander Popel <alexander.popel@postindustria.com>
2025-06-05 16:53:20 +0300

    ci: run ip-intelligence-cxx tests

--- ci/setup-environment.ps1
+++ ci/setup-environment.ps1
@@ -1,3 +1,4 @@
 param($OrgName, $GitHubUser)
-./ip-graph-dotnet/ci/setup-environment.ps1 @PSBoundParameters
-
+$ErrorActionPreference = "Stop"
+./ip-graph-dotnet/ci/setup-environment.ps1 -OrgName $OrgName -GitHubUser $GitHubUser
+./ip-intelligence-cxx/ci/setup-environment.ps1
